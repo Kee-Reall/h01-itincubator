725172a9abf5234eec764c32118d204d2399abc5
Kee-Reall <admin.bezrodnyi@wms_net.local>
2022-11-11 13:35:39 +0500

    #going on

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,7 @@ type storeVideo  = videoModels.StoreVideoModel;
 type createVideo = videoModels.CreateVideoModel;
 type updateVideo = videoModels.UpdateVideoModel;
 type storeVideos = Array<storeVideo>;
+type Istrings = videoModels.Istrings
 
 export class Store {
     state: storeVideos
@@ -78,20 +79,32 @@ export class Store {
         return this.state.find(el => el.id === id)
     }
 
-    checkValidPush(element: createVideo): true | Array<keyof createVideo> {
+    checkValidStringsLength(strings: Istrings):{flag: boolean,errorField: Array<keyof Istrings>} {
         const [maxAuthorLength, maxTittleLength] = [20,40]
-        let flag: boolean = true
-        const errorField: Array<keyof createVideo> = []
-        const availableResolution: string[] = "P144|P240|P360|P480|P720|P1080|P1440|P2160".split("|")
-
-        if (element.author.length > maxAuthorLength || element.author.length <= 0) {
+        let flag = true
+        const errorField: Array<keyof Istrings> = []
+        if (strings.author.length > maxAuthorLength || strings.author.length <= 0) {
             errorField.push("author")
             flag = false
         }
-        if (element.title.length > maxTittleLength || element.title.length <= 0) {
+        if (strings.title.length > maxTittleLength || strings.title.length <= 0) {
             errorField.push("title")
             flag = false
         }
+        return {flag,errorField}
+    }
+
+    checkValidPush(element: createVideo): true | Array<keyof createVideo> {
+        
+        
+        const isStringsValid = this.checkValidStringsLength({
+            author:element.author,
+            title:element.title
+        })
+        let {flag} = isStringsValid
+        const errorField: Array<keyof createVideo> = [...isStringsValid.errorField]
+        
+        const availableResolution: string[] = "P144|P240|P360|P480|P720|P1080|P1440|P2160".split("|")
         if(element.availableResolutions !== null){
             for(let i of element.availableResolutions){
                 // @ts-ignore
@@ -105,7 +118,28 @@ export class Store {
         return flag ? flag : errorField
     }
 
-    checkValidUpdate(obj: updateVideo){
-        //validator of correct update Request
+    checkValidUpdate(updateElement: updateVideo):  true | Array<keyof updateVideo> {
+        const {
+            author, title, canBeDownloaded, minAgeRestriction,
+            publicationDate, availableResolutions
+        } = updateElement
+        const isStringsValid = this.checkValidStringsLength({author,title})
+        let {flag} = isStringsValid
+        const errorField: Array<keyof updateVideo> = [...isStringsValid.errorField]
+        if(typeof canBeDownloaded !== 'boolean'){
+            errorField.push('canBeDownloaded')
+            flag = false
+        }
+        // @ts-ignore
+        if ( minAgeRestriction !== null) {
+            if ( typeof minAgeRestriction !== "number" && (minAgeRestriction > 18 || minAgeRestriction < 0)) {
+                errorField.push('minAgeRestriction')
+                flag = false
+            }  
+        }
+        
+            //validator of correct update Request
+
+        return flag ? flag : errorField
     }
 }
