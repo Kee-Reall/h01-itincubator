65aa64de8bdf76628b0c10e17280d167344320cd
Kee-Reall <kirill_bezrodny@mail.ru>
2022-11-13 22:45:00 +0500

    change index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import app from "./app"
 const port = 3000
 
-try{
-    app.listen(port, (): void => console.info(`Server has been startet on ${port}`)) 
-} catch(e) {
-    console.log("We got some issue")
-}
+app.listen(port, (): void => console.info(`Server has been startet on ${port}`))
